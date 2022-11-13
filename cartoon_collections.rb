8f0ae528e78dc9d16b4ebb09ba05ae62e2fff5a2
Eva Reitzel <evareitzel@gmail.com>
2022-11-13 10:20:28 -0500

    commit tree 1fcbdf45cefd8dc5fb71ebdf18fa29ce9b85591f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves arr # code an argument here
   # Your code here
+  arr.each.with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet planeteer_calls# code an argument here
   # Your code here
+  planeteer_calls.map {|call| "#{call.capitalize()}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls# code an argument here
   # Your code here
+  calls.keep_if {|call| call.length > 4}
+  if calls.empty? 
+    return false
+  end
+  true
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese ingredients# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if ingredients.include? cheese
+      return cheese
+    end
+  end
+  nil
 end
